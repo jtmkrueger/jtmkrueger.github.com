ae4be4f557352809e6eda8fa228f296fa34bf392
John Krueger <jtmkrueger@gmail.com>
2012-05-31 14:10:15 -0600

    city and state showing in legend

--- nrel_demos/dni.js
+++ nrel_demos/dni.js
@@ -2,50 +2,63 @@
 (function() {
 
   navigator.geolocation.getCurrentPosition(function(position) {
-    var startPos;
+    var pos;
     $("#container").html('gathering data...');
-    startPos = position;
-    console.log(startPos);
+    pos = position;
+    console.log(pos);
     return $.ajax({
-      url: 'http://developer.nrel.gov/api/georeserv/service/solar/dni.json',
+      url: 'http://api.geonames.org/findNearbyPlaceNameJSON?',
       data: {
-        api_key: 'cc461d97df558c2937f83c9a94ea539ae2a73a10',
-        lat: startPos.coords.latitude,
-        lon: startPos.coords.longitude
+        username: 'demo',
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude
       },
-      success: function(nrelData) {
-        var chart, dataArr;
-        dataArr = [nrelData.outputs.jan, nrelData.outputs.feb, nrelData.outputs.mar, nrelData.outputs.apr, nrelData.outputs.may, nrelData.outputs.jun, nrelData.outputs.jul, nrelData.outputs.aug, nrelData.outputs.sep, nrelData.outputs.oct, nrelData.outputs.nov, nrelData.outputs.dec];
-        return chart = new Highcharts.Chart({
-          chart: {
-            renderTo: 'container',
-            type: 'column',
-            height: 800,
-            shadow: true,
-            showAxies: true
+      success: function(nameData) {
+        var city, state;
+        city = nameData.geonames[0].name;
+        state = nameData.geonames[0].adminName1;
+        return $.ajax({
+          url: 'http://developer.nrel.gov/api/georeserv/service/solar/dni.json',
+          data: {
+            api_key: 'cc461d97df558c2937f83c9a94ea539ae2a73a10',
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude
           },
-          title: {
-            text: 'Total Direct Normal Solar Resource Values (watt-hours/m2/day) Monthly Average'
-          },
-          xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-          },
-          yAxis: {
-            title: {
-              text: 'watt-hours/m2/day'
-            }
-          },
-          series: [
-            {
-              name: "Latitude: " + startPos.coords.latitude + " | Longitude: " + startPos.coords.longitude,
-              data: dataArr
-            }
-          ]
+          success: function(nrelData) {
+            var chart, dataArr;
+            dataArr = [nrelData.outputs.jan, nrelData.outputs.feb, nrelData.outputs.mar, nrelData.outputs.apr, nrelData.outputs.may, nrelData.outputs.jun, nrelData.outputs.jul, nrelData.outputs.aug, nrelData.outputs.sep, nrelData.outputs.oct, nrelData.outputs.nov, nrelData.outputs.dec];
+            return chart = new Highcharts.Chart({
+              chart: {
+                renderTo: 'container',
+                type: 'column',
+                height: 800,
+                shadow: true,
+                showAxies: true
+              },
+              title: {
+                text: 'Total Direct Normal Solar Resource Values (watt-hours/m2/day) Monthly Average'
+              },
+              xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+              },
+              yAxis: {
+                title: {
+                  text: 'watt-hours/m2/day'
+                }
+              },
+              series: [
+                {
+                  name: "" + city + ", " + state,
+                  data: dataArr
+                }
+              ]
+            });
+          }
+        }, function(error) {
+          return $("#container").html("there was a problem!");
         });
       }
     });
-  }, function(error) {
-    return $("#container").html("there was a problem!");
   });
 
 }).call(this);
